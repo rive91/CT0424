0cfebc67c001f6fcdab0914c46de0d6f01ec8e01
rive <rive@MSI>
2024-04-20 11:19:44 -0600

    Rrimer Commit

---
+++ Curso0424/src/main/java/prueba/Selenium.java
@@ -0,0 +1,15 @@
+package prueba;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		
+
+	}
+
+}
