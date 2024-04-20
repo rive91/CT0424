23987540363e8b740f6506aa80d3b0f42237a8aa
rive91 <57017409+rive91@users.noreply.github.com>
2024-04-20 11:47:33 -0600

    Update Selenium2.java

--- Curso0424/src/main/java/prueba/Selenium.java
+++ Curso0424/src/main/java/prueba/Selenium.java
@@ -8,6 +8,7 @@ public class Selenium {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.twitter.com");
+		driver.get("https://www.twitter.com");
 	}
 
 }
